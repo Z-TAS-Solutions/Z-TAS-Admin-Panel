abaedb919e4424427ded74739966bf2909e7b571
Amna5629 <amnayoosuf123@gmail.com>
2026-03-09 21:44:01 +0530

    routes error try solving

--- src/app/routes.js
+++ src/app/routes.js
@@ -1,8 +1,11 @@
 import { createBrowserRouter } from "react-router";
-import { AdminLayout } from "./components/admin-layout";
-import { Users } from "../pages/users";
-import { Settings } from "../pages/settings";
-import { Alerts } from "../pages/alerts";
+// Match the exact filename: AdminLayout.jsx
+import { AdminLayout } from "./components/AdminLayout"; 
+
+// Match the lowercase filenames: users.jsx, settings.jsx, alerts.jsx
+import { Users } from "./pages/users";
+import { Settings } from "./pages/settings";
+import { Alerts } from "./pages/alerts";
 
 export const router = createBrowserRouter([
   {
