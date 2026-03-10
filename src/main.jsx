6769bdf87b5c80493737d63facc0413bebe81ef3
Rafa Rizvi <rafa.20240499@iit.ac.lk>
2026-03-11 00:42:36 +0530

    App and main.jsx. installed tailwind & did all css styling

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from "react-dom/client";
+import App from "./app/App.jsx";
+import "./styles/index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <h1>Z-TAS Admin Panel</h1>
-  </React.StrictMode>
-);
+createRoot(document.getElementById("root")).render(<App />);
